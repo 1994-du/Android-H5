061b11eae5e7b60347b408cfbaf3feedd0009526
du_1994 <1746215067@qq.com>
2026-03-29 23:17:26 +0800

    fix: 移除VITE_BASE配置并添加代理设置
    移除开发和生产环境中的VITE_BASE配置，因其不再需要。同时在vite配置中添加API代理设置，以解决跨域问题并简化前端请求路径。

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,15 @@ export default defineConfig(({ mode }) => {
     server: {
       host: '0.0.0.0',
       port: 3000,
+      open: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_PROXY,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
     },
   }
 })
